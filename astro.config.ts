9c3b2473b60fb7047008bb9d0800d57942c548d8
Bogdan Radu <bogdanrn@gmail.com>
2025-03-11 19:46:05 +0200

    fix: analytics

--- astro.config.ts
+++ astro.config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
     }),
     ...whenExternalScripts(() =>
       partytown({
-        config: { forward: ['dataLayer.push'] },
+        config: { forward: ['dataLayer.push', 'gtag'] },
       })
     ),
     compress({
